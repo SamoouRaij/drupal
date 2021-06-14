3deeb6df23eea3f9845c2c2056afff2a03094e52
Artem <84867564+SamoouRaij@users.noreply.github.com>
2021-06-15 01:40:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 #Start OC
 if [ "${OSNAME}" = "debian" ]; then
        apt-get -y install git drush
+       apt-get -y install composer
+       composer require drush/drush
    else
       yum -y install git drush
+      yum -y install composer
+      composer require drush/drush
       export VESTA=/usr/local/vesta/
 fi
 #Firs Conf
@@ -40,4 +44,26 @@ cd $SITEDIR
 wget https://ftp.drupal.org/files/projects/drupal-9.1.10.zip
 unzip drupal-9.1.10.zip
 mv drupal-9.1.10.zip/* $SITEDIR/
+chown -R $user. $SITEDIR
+#DATABASE
+DBUSERSUFB="dru";
+i=0;
+while [ $i -lt 99 ]
+do
+i=$((i+1));
+DBUSERSUF="${DBUSERSUFB}${i}";
+DBUSER=$user\_$DBUSERSUF;
+if [ ! -d "/var/lib/mysql/$DBUSER" ]; then
+break;
+fi
+done
+PASSWDDB=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+/usr/local/vesta/bin/v-add-database $user $DBUSERSUF $DBUSERSUF $PASSWDDB mysql
+cd $SITEDIR
+#password="($PASS)"
+password="123"
 
+drush site-install standard --account-name=admin --account-pass=$password --db-url=mysql://$DBUSER:$DBUSER@localhost/$DBUSER -y
+drush dl devel, module_filter, admin_menu
+drush en devel, module_filter, admin_menu -y
+drush dis overlay, toolbar -y
